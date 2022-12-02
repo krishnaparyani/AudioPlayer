5487086c9f0b4d9333bc99b09c057c2603983f29
krishnaparyani <krishnaparyani96@gmail.com>
2022-12-02 17:47:51 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'Kannuna Kalyanam',
+        path: 'musics/Song 1.mp3',
+        artist: 'Anurag Kulkarni, Sinduri S',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'Inthandham',
+        path: 'musics/Song 2.mp3',
+        artist: 'S.P.B Charan',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'Oh Sita',
+        path: 'musics/Song 3.mp3',
+        artist: 'S.P.B Charan, Sinduri Vishal',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: 'Singara Siriye',
+        path: 'musics/Song 4.mp3',
+        artist: 'Vijay Prakash',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: '4 Din',
+        path: 'musics/Song 5.mp3',
+        artist: 'MC Square',
+        cover: 'images/cover 5.png'
+    },
+]
